ecfcbf12cddb2f711bd9142bb1c44dca6d21f43c
Albert Zak <me@albertzak.com>
2017-03-18 12:04:49 +0100

    Attempt to fix license check on CI

---
+++ ci/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "$ANDROID_HOME"
+mkdir "$ANDROID_HOME/licenses" || true
+echo -e "\n8933bad161af4178b1185d1a37fbf41ea5269c55" > "$ANDROID_HOME/licenses/android-sdk-license"
+echo -e "\n84831b9409646a918e30573bab4c9c91346d8abd" > "$ANDROID_HOME/licenses/android-sdk-preview-license"
+
+echo y | android update sdk -u -a -t tools
+echo y | android update sdk -u -a -t platform-tools
+echo y | android update sdk -u -a -t build-tools-19.1.0
+echo y | android update sdk -u -a -t android-9
+echo y | android update sdk -u -a -t extra-google-m2repository
+echo y | android update sdk -u -a -t extra-android-m2repository
