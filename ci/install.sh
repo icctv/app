92f63081ea8a7a3bc1d4b124b011e07972e1165a
Albert Zak <me@albertzak.com>
2017-03-18 14:01:53 +0100

    Push apk to GitHub Releases

--- ci/install.sh
+++ ci/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export ADB_INSTALL_TIMEOUT=5
+
 echo "$ANDROID_HOME"
 mkdir "$ANDROID_HOME/licenses" || true
 echo -e "\n8933bad161af4178b1185d1a37fbf41ea5269c55" > "$ANDROID_HOME/licenses/android-sdk-license"
--- ci/sign.sh
+++ ci/sign.sh
@@ -1,10 +1,40 @@
 #!/bin/bash
 echo "Signing..."
 
-cp $TRAVIS_BUILD_DIR/.keystore $HOME
+if [ -z ${TRAVIS_BUILD_DIR+x} ]; then
+  echo "TRAVIS_BUILD_DIR is not set";
+  exit 1;
+fi
+
+if [ -z ${KEYSTORE_FILE+x} ]; then
+  echo "KEYSTORE_FILE is not set";
+  exit 1;
+fi
+
+if [ -z ${KEYSTORE_PASSWORD+x} ]; then
+  echo "KEYSTORE_PASSWORD is not set";
+  exit 1;
+fi
+
+if [ -z ${KEY_PASSWORD+x} ]; then
+  echo "KEY_PASSWORD is not set";
+  exit 1;
+fi
+
+if [ -z ${KEY_PASSWORD+x} ]; then
+  echo "KEY_PASSWORD is not set";
+  exit 1;
+fi
+
+if [ -z ${APK_NAME+x} ]; then
+  echo "APK_NAME is not set";
+  exit 1;
+fi
+
+cp $TRAVIS_BUILD_DIR/$KEYSTORE_FILE $HOME
 cd app/build/outputs/apk/
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $HOME/keystore.jks -storepass $KEYSTORE_PASSWORD -keypass $KEY_PASSWORD app-release-unsigned.apk $KEY_ALIAS
 
 echo "Verifying signature..."
 jarsigner -verify app-release-unsigned.apk
-"${ANDROID_HOME}/build-tools/21.1.2/zipalign -v 4 app-release-unsigned.apk yourapp.apk"
+"${ANDROID_HOME}/build-tools/21.1.2/zipalign -v 4 app-release-unsigned.apk $APK_NAME"
